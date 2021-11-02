5f6f37ca6ec0d182fc430874f8804d74e6af6651
scottfjennings <scott.f.jennings@gmail.com>
2021-11-02 11:11:21 -0700

    initial commit

---
+++ tests/testthat/test-translate_bird_names.R
@@ -0,0 +1,10 @@
+test_that("translate_bird_names alpha.code to common name and species", {
+  bird_data <- data.frame(alpha.code = c("LTDU", "MALL", "GRSC")) %>%
+    mutate(common.name = translate_bird_names(alpha.code, "alpha.code", "common.name"),
+           species = translate_bird_names(alpha.code, "alpha.code", "species"))
+
+    expect_identical(bird_data, data.frame(alpha.code = c("LTDU", "MALL", "GRSC"),
+                                           common.name = c("Long-tailed Duck", "Mallard", "Greater Scaup"),
+                                           species = c("Clangula hyemalis", "Anas platyrhynchos", "Aythya marila")))
+
+    })
