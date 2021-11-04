481e2ddab7d03ae5222ce6201c160faa64427054
scottfjennings <scott.f.jennings@gmail.com>
2021-11-04 10:23:47 -0700

    detect more non-standard bird names

--- R/bird_names_from_text.R
+++ R/bird_names_from_text.R
@@ -19,10 +19,15 @@
 bird_names_from_text <- function(x) {
 
 
-bird_names <- rbind(full_bird_list %>%
+bird_names <- rbind(combined_bird_list %>%
                       dplyr::select(code.name = .data$common.name),
-                    full_bird_list %>%
-                      dplyr::select(code.name = .data$alpha.code)) %>%
+                    combined_bird_list %>%
+                      dplyr::select(code.name = .data$alpha.code),
+                    combined_bird_list %>%
+                      dplyr::filter(grepl("nidentified", .data$common.name)) %>%
+                      dplyr::mutate(common.name = gsub("Unidentified", "", common.name)) %>%
+                      dplyr::distinct(common.name) %>%
+                      dplyr::rename(code.name = .data$common.name)) %>%
   dplyr::mutate(code.name = tolower(.data$code.name)) %>%
   dplyr::summarise(paste(.data$code.name, collapse = "|"))
 
@@ -31,7 +36,7 @@ out_names <- gsub("c[()]", "", out_names)
 out_names <- gsub("[()]", "", out_names)
 out_names <- gsub(paste(c("character0", '"'), collapse = "|"), "", out_names)
 out_names <- gsub(", ", "_", out_names)
-return(out_names)
+#return(out_names)
 }
 
 # bird_names_from_text(c("Bald Eagle and White-tailed Kite and WREN are birds.", "The Great Blue Heron is taller than the Snowy Egret"))
