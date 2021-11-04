504fe8a1ba9065144d68b5d75e690ce11bd20080
scottfjennings <scott.f.jennings@gmail.com>
2021-11-04 05:57:04 -0700

    updated for combined_bird_list

--- tests/testthat/test-bird_taxa_filter.R
+++ tests/testthat/test-bird_taxa_filter.R
@@ -1,11 +1,14 @@
-test_that("bird_taxa_filter works for Anseriformes", {
 
+
+test_that("bird_taxa_filter works for combined_bird_list", {
   sample_bird_data <- data.frame(alpha.code = c("MALL", "GRSC", "BUFF", "RTHA", "SCAUP",
   "GREG", "HOGR"))
 
-  expect_identical(bird_taxa_filter(sample_bird_data, c("alpha.code", "alpha.code"), "Anseriformes"),
+  expect_identical(bird_taxa_filter(sample_bird_data, c("Anseriformes")),
                    data.frame(alpha.code = c("MALL", "GRSC", "BUFF", "SCAUP"),
                               common.name = c("Mallard", "Greater Scaup", "Bufflehead", "Scaup"),
-                              species = c("Anas platyrhynchos", "Aythya marila", "Bucephala albeola", "Aythya spp.")))
+                              species = c("Anas platyrhynchos", "Aythya marila", "Bucephala albeola", "Aythya spp."),
+                              group.spp = c(NA, NA, NA, "GRSC, LESC")))
 
 })
+
