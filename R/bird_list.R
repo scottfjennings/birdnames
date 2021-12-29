d9da56fd56ce41b90ee7c68a7acb9b2039440730
scottfjennings <scott.f.jennings@gmail.com>
2021-12-29 13:50:36 -0800

    updated documentation

--- R/bird_list.R
+++ R/bird_list.R
@@ -3,8 +3,7 @@
 #' A dataset containing the names and taxonomic information for 2191 species and subspecies of birds found in North America. The dataset contains information from three datasets available online:
 #' https://www.birdpop.org/docs/misc/Alpha_codes_tax.pdf has 4-letter (banding) codes
 #' http://checklist.aou.org/taxa has higher taxonomic classifications
-#' https://www.pwrc.usgs.gov/bbl/manual/speclist.cfm has numeric species codes
-#' These datasets are combined with make_full_bird_list()
+#' These datasets are combined using the workflow in data-raw/bird_list.R
 #'
 #' @format A data frame with 2191 rows and 8 variables:
 #' \describe{
@@ -15,10 +14,11 @@
 #'   \item{subfamily}{The subfamily that the bird species belongs to}
 #'   \item{genus}{The genus that the bird species belongs to}
 #'   \item{species}{The full scientific name (Genus and specific epithet) for the bird species}
-#'   \item{species.number}{The taxonomic number for the bird species (this currenly has many NA)}
+#'   \item{specific.name}{specific epithet (name) for the bird species}
+#'   \item{subspecific.name}{subspecies name for the bird species}
+#'   \item{taxonomic.order}{Numeric helper for sorting species by taxonomic order}
 #'   ...
 #' }
 #' @source \url{https://www.birdpop.org/docs/misc/Alpha_codes_tax.pdf},
-#'         \url{http://checklist.aou.org/taxa},
-#'         \url{https://www.pwrc.usgs.gov/bbl/manual/speclist.cfm}
-"full_bird_list"
+#'         \url{http://checklist.aou.org/taxa}
+"bird_list"
