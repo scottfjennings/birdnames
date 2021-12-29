cbbf08754bfaa00578c92022de56456412bfe071
scottfjennings <scott.f.jennings@gmail.com>
2021-12-29 11:23:11 -0800

    updated to look for custom_bird_list in global environment, and use bird_list if custom_bird_list not found

--- R/translate_bird_names.R
+++ R/translate_bird_names.R
@@ -12,10 +12,15 @@
 #' @param to.name The desired output bird name format. Can be one of c("alpha.code",
 #' "common.name", "order", "family", "subfamily", "genus", "species", "species.number")
 #'
-#' @return character
+#' @return character. If there are no matches for x in bird_list$from.name (or custom_bird_list, if specified),
+#' then returns tolower(x)
 #' @export
 #' @details from.name must be the format of x. E.g., if the x object contains alpha codes,
 #' then from.name should be "alpha.code".
+#'
+#' A custom bird list can be specified as "custom_bird_list" to include user-defined "species" that aren't present in the included "bird_list" dataset. If "custom_bird_list" is not specified, then the included "bird_list" dataset is used as name reference.
+#'
+#'
 #' @examples
 #'
 #' bird_data <- data.frame(alpha.code = c("LTDU", "MALL", "GRSC"))
@@ -27,9 +32,14 @@
 #' bird_data
 translate_bird_names <- function(x, from.name, to.name) {
 
- utils::data("combined_bird_list")
-from_names <- tolower(combined_bird_list[,from.name])
-to_names <- combined_bird_list[,to.name]
+  if(exists("custom_bird_list")) {
+    bird_list = custom_bird_list
+  } else {
+ utils::data("bird_list")
+}
+
+    from_names <- tolower(bird_list[,from.name])
+to_names <- bird_list[,to.name]
 
 plyr::mapvalues(tolower(x), from_names, to_names, warn_missing = FALSE)
 
