7ffa99a3d6e9a6ac0c27787c6d0bf1974b3a84e1
scottfjennings <scott.f.jennings@gmail.com>
2021-12-29 18:02:26 -0800

    updated for simpler function

--- tests/testthat/test-bird_taxa_filter.R
+++ tests/testthat/test-bird_taxa_filter.R
@@ -5,10 +5,7 @@ test_that("bird_taxa_filter works for combined_bird_list", {
   "GREG", "HOGR"))
 
   expect_identical(bird_taxa_filter(sample_bird_data, c("Anseriformes")),
-                   data.frame(alpha.code = c("MALL", "GRSC", "BUFF", "SCAUP"),
-                              common.name = c("Mallard", "Greater Scaup", "Bufflehead", "Scaup"),
-                              species = c("Anas platyrhynchos", "Aythya marila", "Bucephala albeola", "Aythya spp."),
-                              group.spp = c(NA, NA, NA, "GRSC, LESC")))
+                   data.frame(alpha.code = c("MALL", "GRSC", "BUFF")))
 
 })
 
