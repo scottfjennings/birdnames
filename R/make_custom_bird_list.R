b4c8a37834162a898a940a5535cb39c1160dd10e
scottfjennings <scott.f.jennings@gmail.com>
2023-06-16 11:01:33 -0700

    update for more control of manual species .csv

--- R/make_custom_bird_list.R
+++ R/make_custom_bird_list.R
@@ -4,24 +4,25 @@
 #'
 #' Helper function to combine bird group dataset and user-filled dataset.
 #'
+#' @param manual_spp_path file path to your .csv of manual species. should have some/all of these columns: "alpha.code" "common.name" "species" "specific.name" "subspecific.name" "genus" "order" "family" "subfamily" "taxonomic.order" "group.spp"
+#'
 #' @return data frame matching structure of full_bird_list
 #' @importFrom utils read.csv
 #'
 #' @examples
-#' #custom_bird_list <- make_custom_bird_list()
+#' custom_bird_list <- make_custom_bird_list("C:/Users/scott.jennings/OneDrive - Audubon Canyon Ranch/Projects/my_R_general/birdnames_support/data/custom_species.csv")
+#' saveRDS(custom_bird_list, "C:/Users/scott.jennings/OneDrive - Audubon Canyon Ranch/Projects/my_R_general/birdnames_support/data/custom_bird_list")
 
-make_custom_bird_list <- function(){
-      #full_bird_list1 <- read.csv("C:/Users/scott.jennings/Documents/Projects/R_general/utility_functions/utility_function_data/bird_taxa_list.csv") %>% dplyr::select(-taxonomic.order)
-bird_lumpies <- utils::read.csv("C:/Users/scott.jennings/Documents/Projects/R_general/utility_functions/utility_function_data/bird_lumpies.csv")
+make_custom_bird_list <- function(manual_spp_path){
 
-manual_species <- utils::read.csv("C:/Users/scott.jennings/Documents/Projects/R_general/utility_functions/utility_function_data/manual_species.csv") %>%
-  dplyr::mutate(group.spp = NA)
+data("bird_list")
+manual_species <- utils::read.csv(manual_spp_path)
 
-custom_bird_list <- rbind(bird_lumpies, manual_species) %>%
-  dplyr::mutate_if(is.factor, as.character) %>%
-  dplyr::filter(!is.na(.data$species)) %>%
-  dplyr::distinct() %>%
-  dplyr::mutate(species = ifelse(.data$alpha.code == "TURN", "Arenaria spp.", .data$species))
+custom_bird_list <- dplyr::bind_rows(bird_list, manual_species)
+custom_bird_list <- dplyr::mutate_if(custom_bird_list, is.factor, as.character)
+custom_bird_list <- dplyr::distinct(custom_bird_list)
+custom_bird_list <- dplyr::mutate(custom_bird_list, species = ifelse(.data$alpha.code == "TURN", "Arenaria spp.", .data$species))
+
 return(custom_bird_list)
 }
 
